6e9829923a9c040a94cbd0e1fcd20728b2c77c9a
ISA VESIT <76900795+ISA-VESIT@users.noreply.github.com>
2021-04-17 08:47:50 +0530

    Create app.js

---
+++ Session 1/app.js
@@ -0,0 +1,38 @@
+// Initialize the map
+// const map = L.map('map',{zoom: 4.8}).setView([23.2599, 77.4126]);
+
+// const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+// const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+// const tileLayer = L.tileLayer(tileUrl, { attribution });
+// tileLayer.addTo(map);
+
+// var mumbai = L.marker ([18.9389, 72.8258]);  // lat, long
+// mumbai.addTo(map);
+
+// var ahmedabad = L.marker ([23.0918, 72.5975]);
+// ahmedabad.addTo(map);
+
+// var delhi = L.marker ([28.6379, 77.2432]);
+// delhi.addTo(map);
+
+// var kolkata = L.marker ([22.5646, 88.3433]);
+// kolkata.addTo(map);
+
+// var bangalore = L.marker ([12.9788, 77.5996]);
+// bangalore.addTo(map);
+
+// var chennai = L.marker ([13.0629, 80.2792]);
+// chennai.addTo(map);
+
+// var line = L.polyline ([[18.9389, 72.8258], [23.0918, 72.5975], [28.6379, 77.2432], [22.5646, 88.3433], [13.0629, 80.2792], [12.9788, 77.5996] ]);
+// line.addTo(map);
+
+// mumbai.bindPopup ('Wankhede Stadium, Mumbai');
+// ahmedabad.bindPopup ("Narendra Modi Stadium, Ahmedabad");
+// delhi.bindPopup ("Feroz Shah Kotla, Delhi");
+// kolkata.bindPopup ("Eden Gardens, Kolkata");
+// bangalore.bindPopup ("Chinnaswamy Stadium, Bangalore");
+// chennai.bindPopup ("Chidambaram Stadium, Chennai");
+
+// var current_position = L.circle ([18.9389, 72.8258], {radius: 50000, color:'#008000'});  // meters
+// current_position.addTo(map);
